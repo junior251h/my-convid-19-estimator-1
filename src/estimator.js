afa9be7adf6b11bc09b999f83c9d228b5ba0ccb3
JUNIOR <50843343+junior251h@users.noreply.github.com>
2020-04-11 23:31:33 +0200

    start building assessment

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,18 @@
 const covid19ImpactEstimator = (data) => {
     const Input = data;
 
+    const beds = (0.35 * data.totalHospitalBeds);
+    const income = data.region.avgDailyIncomeInUSD;
+    const population = data.region.avgDailyIncomePopulation;
+
+// Normalize timeToElapse to days 
+
+    if (data.periodType === 'weeks') {
+        data.timeToElapse *= 7;
+    } else if (data.periodType === 'months') {
+        data.timeToElapse *= 30;
+    }
+
     const days = data.timeToElapse;
     const factor = Math.trunc(days / 3);
     impact.currentlyInfected = data.reportedCases * 10;
@@ -11,11 +23,19 @@ const covid19ImpactEstimator = (data) => {
     impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
     severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
 
-    if (data.periodType === 'weeks') {
-        data.timeToElapse *= 7;
-    } else if (data.periodType === 'months') {
-        data.timeToElapse *= 30;
-    }
+    const severeCases = severeImpact.severeCasesByRequestedTime;
+    impact.hospitalBedsByRequestedTime = Math.ceil(beds - impact.severeCasesByRequestedTime);
+    severeImpact.hospitalBedsByRequestedTime = Math.ceil(beds - severeCases);
+
+    impact.casesForICUByRequestedTime = 0.05 * impact.infectionsByRequestedTime;
+    severeImpact.casesForICUByRequestedTime = 0.05 * severeImpact.infectionsByRequestedTime;
+
+    impact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
+    severeImpact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
+
+    const severeInfectionsByRequestedTime = severeImpact.infectionsByRequestedTime;
+    impact.dollarInFlight = impact.infectionsByRequestedTime * income * population * days;
+    severeImpact.dollarInFlight = severeInfectionsByRequestedTime * income * population * days;
 
   
     return {
